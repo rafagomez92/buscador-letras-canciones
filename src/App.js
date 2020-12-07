c89c32cb52863a355b5fd6167f9c24d158f4ee90
rafagomez92 <rafael_gomez92@hotmail.com>
2020-12-07 16:32:12 -0600

    modificaciones de vista

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
-import { useEffect, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
-import axios from 'axios';
 import Cancion from './components/Cancion';
 import Informacion from './components/Informacion';
+import axios from 'axios';
 
 function App() {
 
-  const [ busquedaLetra, guardarBusquedaLetra ] = useState({});
-  const [ letra, guardarLetra ] = useState('');
-  const [ info, guardarInfo ] = useState({});
+  // definir el state
+  const [ busquedaletra, guardarBusquedaLetra ] = useState({});
+  const [ letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
 
-  
   useEffect(() => {
-    if(Object.keys(busquedaLetra).length === 0) return;
+    if(Object.keys(busquedaletra).length === 0 ) return;
+
     const consultarApiLetra = async () => {
-      const { artista, cancion } = busquedaLetra;
-      const URL = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
-      const URLArtista = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      const [ letra, informacion ] = await Promise.all([
-        axios(URL),
-        axios(URLArtista)
-      ])           
-      
-      guardarLetra(letra.data.lyrics);                  
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ]);
+
+      guardarLetra(letra.data.lyrics);
       guardarInfo(informacion.data.artists[0]);
+      
     }
-
     consultarApiLetra();
+  }, [busquedaletra, info]);
 
-  }, [busquedaLetra, info]);
-
-  console.log(letra);
-  console.log(info);
   return (
-    <>      
-      <Formulario 
-        guardarBusquedaLetra={guardarBusquedaLetra}
-      />
+      <Fragment>
+          <Formulario 
+            guardarBusquedaLetra={guardarBusquedaLetra}
+          />
 
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-md-6">
-            <Informacion info={info} />
+          <div className="container mt-5">
+            <div className="row">
+              <div className="col-md-6">
+                <Informacion 
+                  info={info}
+                />
+              </div>
+              <div className="col-md-6">
+                  <Cancion 
+                    letra={letra}
+                  />
+              </div>
+            </div>
           </div>
-          <div className="col-md-6">
-            <Cancion letra={letra} />
-          </div>
-        </div>        
-      </div>
-    </>
+      </Fragment>
   );
 }
 
--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -1,11 +1,15 @@
-const Cancion = ({ letra }) => {
+import React, { Fragment } from 'react';
+
+const Cancion = ({letra}) => {
+
     if(letra.length === 0) return null;
-    return (
-        <>
+
+    return ( 
+        <Fragment>
             <h2>Letra Canción</h2>
             <p className="letra">{letra}</p>
-        </>
+        </Fragment>
     );
-};
+}
  
 export default Cancion;
